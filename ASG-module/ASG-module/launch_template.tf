db39706136c9e9e18a5dca681606507a3350ed47
RGSMAD <girisaimadhav@gmail.com>
2026-04-09 16:06:44 +0530

    ASG-module updated

--- ASG-module/ASG-module/launch_template.tf
+++
@@ -1,9 +0,0 @@
-# Define a Launch Template
-resource "aws_launch_template" "sample_template" {
-  name_prefix   = var.name_prefix
-  image_id      = var.ami-id
-  instance_type = var.instance_type
-  vpc_security_group_ids = var.sg-id
-  key_name = var.key_name
-  user_data = var.user_data
-}
--- ASG-module/ASG-module/load_balancer.tf
+++
@@ -1,8 +0,0 @@
-# Define Application Load Balancer
-resource "aws_lb" "sample_alb" {
-  name               = var.alb_name
-  internal           = var.scheme
-  load_balancer_type = "application"
-  security_groups    = var.sg-id
-  subnets            = var.subnet_ids
-}
