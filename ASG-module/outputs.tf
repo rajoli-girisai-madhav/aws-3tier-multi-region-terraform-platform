c274583300e5362ebf798c085f289e30536aa677
RGSMadhav <123295059+RGSMAD@users.noreply.github.com>
2026-04-10 12:10:12 +0530

    Update outputs.tf

--- ASG-module/outputs.tf
+++ ASG-module/outputs.tf
@@ -6,3 +6,16 @@ output "ASG_id" {
 output "asg_name" {
   value = aws_autoscaling_group.sample_ASG.name
 }
+
+output "alb_dns_name" {
+  value       = aws_lb.sample_alb.dns_name
+  description = "ALB DNS name"
+}
+
+output "alb_arn" {
+  value = aws_lb.sample_alb.arn
+}
+
+output "target_group_arn" {
+  value = aws_lb_target_group.sample_tg.arn
+}
